9d626ab06bfdfb1adecc6980e848d13af0f88523
xXxOHTOHxXx <83760285+xXxOHTOHxXx@users.noreply.github.com>
2024-08-19 13:01:34 +0300

    Add project files.

---
+++ WEB_253502_Garnik/Controllers/Home.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_253502_Garnik.Controllers {
+    public class Home : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+        public IActionResult Lab1() {
+            return View("~/Views/Lab1/Lab1.cshtml");
+        }
+    }
+}
---
+++ WEB_253502_Garnik/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace WEB_253502_Garnik.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
